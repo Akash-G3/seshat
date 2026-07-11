9b7c403a245cf2c67ec4ccd5266923d6190f6214
Akash-G3 <thisisakash481@gmail.com>
2026-07-11 04:24:03 +0000

    feat: Colorize terminal through colorette

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,21 +4,37 @@ import util from 'util';
 import path from 'node:path';
 import { createLogger, format, transports } from 'winston';
 import { ConsoleTransportInstance, FileTransportInstance } from 'winston/lib/winston/transports';
+import { red, blue, yellow, green, magenta } from 'colorette';
 import * as sourceMapSupport from 'source-map-support';
 
 //Linking trace support  [source map]
-sourceMapSupport.install()
+sourceMapSupport.install();
+
+//Colorette fn for colorized terminal
+const colorizeLevel = (level: string) => {
+  switch (level) {
+    case 'ERROR':
+      return red(level);
+    case 'INFO':
+      return blue(level);
+    case 'WARN':
+      return yellow(level);
+    default:
+      return level;
+  }
+};
 
 const consoleLogFormat = format.printf((info) => {
   const { level, message, timestamp, meta = {} } = info;
-  const customLevel = level.toUpperCase();
-  const customTimestamp = timestamp;
+  const customLevel = colorizeLevel(level.toUpperCase());
+  const customTimestamp = green(timestamp as string);
   const customMessage = message;
   const customMeta = util.inspect(meta, {
     showHidden: false,
     depth: null,
+    colors: true,
   });
-  const customLog = `${customLevel} [${customTimestamp}] ${customMessage}\n${'META'} ${customMeta}\n`;
+  const customLog = `${customLevel} [${customTimestamp}] ${customMessage}\n${magenta('META')} ${customMeta}\n`;
   return customLog;
 });
 
